ed4691ef3464b83fdfee65c4a2d31fee1b119789
moabarar <moab.arar@gmail.com>
2019-12-22 13:30:32 +0200

    Clean version - Initial Commit.

--- train.py
+++ train.py
@@ -27,7 +27,6 @@ from util.visualizer import Visualizer
 
 if __name__ == '__main__':
     print('Reading Refernce Points:')
-    reference_points = read_reference_points('train_points.csv')
     opt = TrainOptions().parse()  # get training options
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     dataset_size = len(dataset)  # get the number of images in the dataset.
